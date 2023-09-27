e2ba148b7f5f0bfc4a9904550188ce6c59e46016
pedro123 <pedro@gmail.com>
2023-09-27 12:58:04 -0300

    Upgrade Forms 5

--- Cadastro.php
+++ Cadastro.php
@@ -4,50 +4,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="teste.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
-<body>
-    <nav class="nav bg-dark">
-        <nav class="navbar text-white">
-            <div class="md-5">Produtos</div>
-        </nav>
-    </nav>
-
-    <form action="#" method="POST">
-        <div class="col-3 mb-4">
-            <label for="nameInput" class="form-label">Nome</label>
-            <input class="form-control form-control-lg" type="text" placeholder="Digite seu nome" aria-label=".form-control-lg example" id="nameInput" name="nome">
-        </div>
-        <div class="col-3 mb-4">
-            <label for="userInput" class="form-label">Username</label>
-            <input class="form-control form-control-lg" type="text" placeholder="Digite seu Username" aria-label=".form-control-lg example" id="userInput" name="username">
-        </div>
-        <div class="form-check col-3 mb-4">
-            <input class="form-check-input" type="radio" name="boxAdmin" id="flexRadioDefault1">
-            <label class="form-check-label" for="flexRadioDefault1">
-                Conta de Adiministrador
-            </label>
+<body >
+    <div class="teste">
+        <form action="#" method="POST" class="row g-3 d-flex justify-content-center">
+            <div class="col-10 form-floating" >
+                <input class="form-control" type="text" placeholder="name@example.com"  id="nameInput" name="nome">
+                <label for="nameInput">Nome</label>
             </div>
-            <div class="form-check col-3 mb-4">
-            <input class="form-check-input" type="radio" name="boxComum" id="flexRadioDefault2" checked>
-            <label class="form-check-label" for="flexRadioDefault2">
-                Conta Comum
-            </label>
-        </div>
-        <div class="col-3 mb-4">
-            <label for="exampleInputEmail1" class="form-label">Endereço de Email</label>
-            <input type="email" class="form-control form-control-lg" placeholder="Digite seu email" id="exampleInputEmail1" name="email">
-        </div>
-        <div class="col-3 mb-4">
-            <label for="exampleInputPassword1" class="form-label">Senha</label>
-            <input type="password" class="form-control form-control-lg" placeholder="Digite sua senha" id="exampleInputPassword1" name="senha">
-        </div>
-        <div class="col-3 mb-4">
-            <label for="exampleInputPassword2" class="form-label">Confirmar senha</label>
-            <input type="password" class="form-control form-control-lg" placeholder="Confirme sua senha" id="exampleInputPassword2" name="confirmaSenha">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+
+            <div class="col-10 form-floating ">
+                <input class="form-control " type="text" placeholder="Digite seu Username" id="userInput" name="username">
+                <label for="userInput" class="form-label">Username</label>
+            </div>
+
+            <div class="col-md-5 form-floating">
+                <input type="email" class="form-control" placeholder="Digite seu email" id="exampleInputEmail1" name="email">
+                <label for="exampleInputEmail1" class="form-label">Endereço de Email</label>
+            </div>
+            <div class="col-md-5 form-floating">
+                <input type="password" class="form-control" placeholder="Digite sua senha" id="exampleInputPassword1" name="senha">
+                <label for="exampleInputPassword1" class="form-label">Senha</label>
+            </div>
+
+            <div class="form-check col-md-5">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Conta Adiministrador
+                </label>
+            </div>
+
+            <div class="form-check col-md-4">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                <label class="form-check-label" for="flexRadioDefault2">
+                    Conta Comum
+                </label>
+            </div>
+
+            <div class="col-10">
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>    
+    
 </body>
 </html>
