85b73e3c5e29f6336fc1f967b1b275207b7c7860
lusca <106847107+stlucsta@users.noreply.github.com>
2023-09-28 09:58:34 -0300

    Add files via upload

---
+++ Cadastro_produtos.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <title>Cadastro de produtos</title>
+</head>
+<body>
+  <h3>Complete as informações abaixo para cadastrar produto:</h3></br>
+
+  <div class="row g-3">
+  <div class="col-sm-2">
+    <input type="text" class="form-control" placeholder="Nome" aria-label="Nome">
+  </div>
+  <div class="col-sm-2">
+    <input type="text" class="form-control" placeholder="Descrição" aria-label="Descrição">
+  </div>
+  <div class="col-sm-2">
+    <input type="text" class="form-control" placeholder="Preço" aria-label="Preço">
+  </div>
+  <div class="col-sm-2">
+    <input type="text" class="form-control" placeholder="Tipo_ID" aria-label="Tipo_ID">
+  </div>
+  <div class="col-auto">
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+</div>
+
+</body>
+</html>
